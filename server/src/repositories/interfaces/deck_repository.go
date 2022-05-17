d2b3700d33260f4946d55a8f4ef12dfcd023fe0d
hericles.koelher <brunokoelher@hotmail.com>
2022-05-17 18:29:10 -0300

    implementei a inserção e leitura e corrigi o metodo de deletar

--- server/src/repositories/interfaces/deck_repository.go
+++ server/src/repositories/interfaces/deck_repository.go
@@ -3,8 +3,7 @@ package repositories
 import "server/src/models"
 
 type DeckRepository interface {
-	Create() (*models.Deck, error)
-	Read(uuid string) (*models.Deck, error)
-	Update(deck map[string]interface{}) (*models.Deck, error)
 	Delete(uuid string) error
+	InsertOrUpdate(deck *models.Deck) (*models.Deck, error)
+	Read(uuid string) (*models.Deck, error)
 }
--- server/src/repositories/mongo_deck_repository.go
+++ server/src/repositories/mongo_deck_repository.go
@@ -4,71 +4,77 @@ import (
 	"context"
 	"fmt"
 	"server/src/models"
-	"time"
 
-	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// TODO: Mudar o uso de context nesse arquivo. Devemos conversar sobre isso depois...
+
+// TODO: Melhorar todos os tratamentos de erro.
+
 type MongoDeckRepository struct {
-	// Só pode ser acessado pelo pacote, pois começa com
-	// letra minuscula.
-	deckCollection *mongo.Collection
+	// Só pode ser acessado pelo pacote, pois começa com letra minuscula.
+	coll *mongo.Collection
 }
 
 func NewMongoDeckRepository(collection *mongo.Collection) *MongoDeckRepository {
 	repository := new(MongoDeckRepository)
 
-	repository.deckCollection = collection
+	repository.coll = collection
 
 	return repository
 }
 
-func (repository MongoDeckRepository) Create() (*models.Deck, error) {
-	deckUuid := uuid.New()
-
-	fmt.Println("Generated UUID = " + deckUuid.String())
-
-	deck := &models.Deck{
-		UUID:             deckUuid.String(),
-		LastModification: time.Now().UnixMilli(),
-		IsPublic:         false,
-	}
-
-	// TODO: pesquisar o funcionamento desse context
-	context, _ := context.WithTimeout(context.Background(), 15*time.Second)
-
-	_, insertErr := repository.deckCollection.InsertOne(context, deck)
-
-	// TODO: melhorar essa parte aqui...
-	if insertErr != nil {
-		fmt.Println("Erro de inserção...")
-
-		return nil, insertErr
-	}
-
-	return deck, nil
-}
-
-func (repository MongoDeckRepository) Read(uuid string) (*models.Deck, error) {
-	// TODO: retornar baralho com o uuid especificado.
-}
-
-func (repository MongoDeckRepository) Update(deck map[string]interface{}) (*models.Deck, error) {
-	// TODO: atualizar baralho com o uuid especificado.
-}
-
 func (repository MongoDeckRepository) Delete(uuid string) error {
-	// TODO: deletar baralho com o uuid especificado.
-	// TODO: pesquisar o funcionamento desse context
-	context, _ := context.WithTimeout(context.Background(), 15*time.Second)
-
-	_, err := repository.deckCollection.DeleteOne(context, bson.M{"UUID": uuid})
+	_, err := repository.coll.DeleteOne(
+		context.TODO(),
+		bson.M{"UUID": uuid},
+	)
 
 	if err != nil {
 		fmt.Println("Erro ao deletar baralho...")
 
 		return err
+	} else {
+		return nil
+	}
+}
+
+func (repository MongoDeckRepository) InsertOrUpdate(deck *models.Deck) (*models.Deck, error) {
+	// Flag que indica que caso o baralho não exista, então ele será inserido...
+	upsert := true
+
+	_, err := repository.coll.UpdateOne(
+		context.TODO(),
+		bson.M{"_id": deck.UUID},
+		deck,
+		&options.UpdateOptions{Upsert: &upsert},
+	)
+
+	if err != nil {
+		fmt.Println("Erro de inserção...")
+
+		return nil, err
+	} else {
+		return deck, nil
+	}
+}
+
+func (repository MongoDeckRepository) Read(uuid string) (*models.Deck, error) {
+	result := new(models.Deck)
+
+	err := repository.coll.FindOne(
+		context.TODO(),
+		bson.M{"_id": uuid},
+	).Decode(result)
+
+	if err != nil {
+		fmt.Println("Erro de leitura...")
+
+		return nil, err
+	} else {
+		return result, nil
 	}
 }
