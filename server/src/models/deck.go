98441ec920a0d611670b2e82e3e29f4466e8e5e8
hericles.koelher <brunokoelher@hotmail.com>
2022-06-10 20:38:00 -0300

    adição do campo descrição ao baralho

--- server/src/models/deck.go
+++ server/src/models/deck.go
@@ -1,9 +1,10 @@
 package models
 
 type Deck struct {
-	Name             string `bson:"name,omitempty"`
 	Cards            []Card `bson:"cards,omitempty"`
-	LastModification int64  `bson:"lastModification,omitempty"`
+	Description      string `bson:"description,omitempty"`
 	IsPublic         bool   `bson:"isPublic,omitempty"`
+	LastModification int64  `bson:"lastModification,omitempty"`
+	Name             string `bson:"name,omitempty"`
 	UUID             string `bson:"_id"`
 }
