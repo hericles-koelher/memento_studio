a5d1041c7d6527714ac1958eb65a9700656611dd
mikaellafs <mikaellaferreira0@gmail.com>
2022-06-22 23:51:09 -0300

    Corrige teste de post de baralho pra nova forma de post

--- server/tests/controllers/deck_controller_test.go
+++ server/tests/controllers/deck_controller_test.go
@@ -1,17 +1,19 @@
 package controllers_tests
 
 import (
+	// "io"
+	"os"
 	"testing"
 	"fmt"
 	"bytes"
 	"net/http"
+	"path/filepath"
 	"encoding/json"
+	"mime/multipart"
 
 	"server/src/models"
 	
 	"github.com/stretchr/testify/assert"
-	
-	// "io"
 )
 
 func TestDeleteDeck(t *testing.T) {
@@ -26,27 +28,65 @@ func TestDeleteDeck(t *testing.T) {
 }
 
 func TestPostDecks(t *testing.T) {
-	decks := []models.Deck {
-		models.Deck {
-			Name:             "Test 1 Deck",
-			Cards:            []models.Card{},
-			LastModification: 1654638124,
-			IsPublic:         false,
-			UUID:             "testdeckid1",
+	cards := []models.Card{
+		models.Card {
+			FrontText: 	"Texto da frente do card 1",
+			BackText:	"Texto de trás do card 1",
+			UUID:		"testecardid1",
 		},
-		models.Deck {
-			Name:             "Test 2 Deck",
-			Cards:            []models.Card{},
-			LastModification: 1654638124,
-			IsPublic:         true,
-			UUID:             "testdeckid2",
+		models.Card {
+			FrontText: 	"Texto da frente do card 2",
+			BackText:	"Texto de trás do card 2",
+			UUID:		"testecardid2",
 		},
 	}
 
-	bodyBytes, _ := json.Marshal(decks)
-	bodyBuffer := bytes.NewBuffer(bodyBytes)
+	deck := models.Deck {
+			Name:             "Test 1 Deck",
+			Cards:            cards,
+			LastModification: 1654638124,
+			IsPublic:         false,
+			UUID:             "testdeckid1",
+		}
+	
+	// Create form
+	bodyBuffer := new(bytes.Buffer)
+	w := multipart.NewWriter(bodyBuffer)
+	
+	deckBytes, _ := json.Marshal(deck)
+	formField, _ := w.CreateFormField("deck")
+	formField.Write(deckBytes)
+
+	absPath, _ := filepath.Abs("../imagesfortest/geraldin.jpg")
+	imageBytes, err := os.ReadFile(absPath)
+	formField, _ = w.CreateFormField("deck-image")
+	formField.Write(imageBytes)
+
+	absPath, _ = filepath.Abs("../imagesfortest/laika.jpg")
+	imageBytes, err = os.ReadFile(absPath)
+	formField, _ = w.CreateFormField("card-front-1")
+	formField.Write(imageBytes)
+	
+	absPath, _ = filepath.Abs("../imagesfortest/sushiraldo.jpg")
+	imageBytes, err = os.ReadFile(absPath)
+	formField, _ = w.CreateFormField("card-front-2")
+	formField.Write(imageBytes)
+	
+	absPath, _ = filepath.Abs("../imagesfortest/tirinha.jpg")
+	imageBytes, err = os.ReadFile(absPath)
+	formField, _ = w.CreateFormField("card-back-1")
+	formField.Write(imageBytes)
+	
+	absPath, _ = filepath.Abs("../imagesfortest/meme.jpg")
+	imageBytes, err = os.ReadFile(absPath)
+	formField, _ = w.CreateFormField("card-back-2")
+	formField.Write(imageBytes)
+
+	w.Close()
 
 	request, err := http.NewRequest(http.MethodPost, "/api/decks", bodyBuffer)
+	request.Header.Add("Content-Type", w.FormDataContentType())
+
 	if err != nil {
 		t.FailNow()
 	}
