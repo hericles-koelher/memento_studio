de36e8978fd599b85e443d9f48bd1d22d2f9d72a
hericles.koelher <brunokoelher@hotmail.com>
2022-07-01 19:41:47 -0300

    comecei a implementar a autenticação

---
+++ app/lib/src/exceptions/m_s_auth_exception.dart
@@ -0,0 +1,21 @@
+import 'package:memento_studio/src/exceptions.dart';
+
+enum MSAuthExceptionCode {
+  invalidEmail,
+  disableAccount,
+  accountNotFound,
+  wrongPassword,
+  accountExistsWithDifferentCredential,
+  emailAlreadyInUse,
+  weakPassword,
+  unknown,
+}
+
+class MSAuthException extends MSBaseException {
+  final MSAuthExceptionCode code;
+
+  MSAuthException({
+    required String message,
+    required this.code,
+  }) : super(message: message);
+}
