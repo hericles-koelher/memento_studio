ef4fae397250ad51f0d4302a6a22964386a9c9da
mikaellafs <mikaellaferreira0@gmail.com>
2022-07-08 00:00:27 -0300

    Cria repositório de baralho - Get implementado - Tentei fazer um result pra indicar sucesso ou falha pra usar no retorno - Falta implementar o resto :(

---
+++ app/lib/src/repositories/interfaces/deck_repository_interface.dart
@@ -0,0 +1,11 @@
+abstract class DeckRepositoryInterface {
+  dynamic getDecks(int page, int pageSize);
+
+  // dynamic saveDeck();
+
+  // dynamic updateDeck();
+
+  // dynamic deleteDeck();
+
+  // dynamic copyDeck();
+}
