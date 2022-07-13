3df6fcbc026ee0e396b85b999cb190b8200afbed
mikaellafs <mikaellaferreira0@gmail.com>
2022-07-13 19:46:12 -0300

    Implementa get de baralho público em repositório de referências

--- app/lib/src/repositories/deck_reference_repository.dart
+++ app/lib/src/repositories/deck_reference_repository.dart
@@ -3,14 +3,17 @@ import '../entities/local/result.dart';
 import 'interfaces/deck_reference_repository_interface.dart';
 import 'package:memento_studio/src/entities/local/result.dart';
 import 'package:memento_studio/src/entities/local/deck_reference.dart' as local_model;
+import 'package:memento_studio/src/entities/local/deck.dart';
 
 typedef DeckListResult = Result<List<local_model.DeckReference>>;
+typedef DeckResult = Result<Deck>;
 
 class DeckReferenceRepository extends DeckReferenceRepositoryInterface {
   DeckReferenceApi api;
 
   DeckReferenceRepository(this.api);
 
+  @override
   Future<DeckListResult> getDecks(int page, int pageSize, Map<String, dynamic>? filter) async {
     final response = await api.getDecks(page, pageSize, filter);
 
@@ -24,7 +27,20 @@ class DeckReferenceRepository extends DeckReferenceRepositoryInterface {
     }
   }
 
-  // Future<DeckResult> getDeck(String id) async {
+  @override
+  Future<DeckResult> getDeck(String id) async {
+    final response = await api.getDeck(id);
 
-  // }
+    if (!response.isSuccessful) {
+      return Error(Exception(response.error.toString()));
+    } else {
+      final deck = response.body?.toDomainModel();
+
+      if (deck == null) {
+        return Error(Exception("Could not parse response body to deck model"));
+      }
+      
+      return Success(deck);
+    }
+  }
 }
