a9536714da395d1d43cea10ef8b7f350473d1024
mikaellafs <mikaellaferreira0@gmail.com>
2022-07-31 19:16:02 -0300

    Cria widget de pesquisa com tags

---
+++ app/lib/src/widgets/textfield_tags.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class TextFieldTags extends StatelessWidget {
+  final List<String> tags;
+  void Function(String, List<String>) onSearchAction;
+  void Function(String)? onAddTag;
+  void Function(String)? onDeleteTag;
+
+  final textFieldController = TextEditingController();
+
+  TextFieldTags(
+      {Key? key,
+      this.tags = const <String>[],
+      required this.onSearchAction,
+      this.onDeleteTag,
+      this.onAddTag})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          controller: textFieldController,
+          decoration: InputDecoration(
+            labelText: "Pesquisar",
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            suffixIcon: IconButton(
+              onPressed: () {
+                onSearchAction(textFieldController.text, tags);
+              },
+              icon: const Icon(Icons.search),
+            ),
+            border: const OutlineInputBorder(),
+          ),
+        ),
+        tags.isEmpty
+            ? Container()
+            : SizedBox(
+                height: 50,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: tags.length,
+                  itemBuilder: (_, index) => Chip(
+                    label: Text(tags[index]),
+                    onDeleted: () {
+                      print(tags[index]);
+                      if (onDeleteTag != null) onDeleteTag!(tags[index]);
+                    },
+                  ),
+                  separatorBuilder: (_, __) => const SizedBox(width: 5),
+                ),
+              ),
+        TextButton(
+          onPressed: () {
+            if (onAddTag != null) onAddTag!(textFieldController.text);
+          },
+          child: const Text("Adicionar tag"),
+        )
+      ],
+    );
+  }
+}
